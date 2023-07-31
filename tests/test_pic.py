e949af16bb6d50818ba22739c5def196afda09b1
Dmitriy <random10208@mail.ru>
2023-07-31 23:59:55 +0300

    сделаны фикстуры

--- tests/test_pic.py
+++ tests/test_pic.py
@@ -1,33 +1,18 @@
 from tests.page_yandex import MainPage
-from selenium import webdriver
-
-page = MainPage(webdriver.Chrome(), "https://ya.ru/")
-page.implicitly_wait(5)
-page.get_url()
-page.maximize_window()
-
-# переменные для проверок
-first_category = ""
-first_pic_name = ""
-second_pic_name = ""
-back_pic_name = ""
 
 
-def test_search_field_click():
-    """клик по полю поиска, чтобы появилось быстрое меню"""
+def test_pic(web_browser):
+    page = MainPage(web_browser, "https://ya.ru/")
+    page.get_url()
 
     # Проверить, что кнопка меню присутствует на странице
     page.search_field().click()  # клик по полю поиска, чтобы появилось быстрое меню
     page.find_menu()
 
-
-def test_menu_click():
     """клик на кнопку меню"""
     assert page.menu().is_displayed(), "Кнопка меню отсутствует на первоначальной странице"
     page.menu().click()
 
-
-def test_element_pic_click():
     """клик на кнопку картинки"""
     page.element_pic().click()
     page.new_window()
@@ -37,7 +22,6 @@ def test_element_pic_click():
     current_url = page.get_current_url()
     assert current_url == expected_url, "Мы не перешли на url https://yandex.ru/images/"
 
-    global first_category
     first_category = page.category_name()  # получили название первой категории
 
     page.first_pic_category().click()
@@ -45,35 +29,26 @@ def test_element_pic_click():
     element_get_content = page.pic_search_field()
     assert first_category == element_get_content, "Названия первой категории нет в поле поиска"
 
-
-def test_first_pic_click():
     """клик на первую картинку"""
     page.first_pic().click()
     # Проверяем, что картинка открылась
     assert page.pic_preview().is_displayed(), "Картинка не открылась"
 
     # запомним имя первой картинки
-    global first_pic_name
     first_pic_name = page.pic_name()
 
-
-def test_next_button_click():
     """клик на кнопку далее"""
     page.next_button().click()
 
     # запомним имя второй картинки
-    global second_pic_name
     second_pic_name = page.pic_name()
     # Проверяем, что картинка сменилась
     assert second_pic_name != first_pic_name, "Картинка не сменилась"
 
-
-def test_prev_button_click():
     """клик на кнопку назад"""
     page.prev_button().click()
 
     # запомним имя картинки после возврата
-    global back_pic_name
     back_pic_name = page.pic_name()
     # Проверяем, что картинка осталась из шага 8
     assert back_pic_name == first_pic_name, "Картинка не такая же, какая была первой"
--- tests/test_search.py
+++ tests/test_search.py
@@ -1,36 +1,28 @@
 from tests.page_yandex import MainPage
 from selenium import webdriver
 
-page = MainPage(webdriver.Chrome(), "https://ya.ru/")
-page.implicitly_wait(5)
-page.maximize_window()
-page.get_url()
 
+def test_search(web_browser):
+    page = MainPage(webdriver.Chrome(), "https://ya.ru/")
 
-def test_find_search_field():
+    page.get_url()
+
     # Проверить наличие поля поиска
     page.find_search_field()
 
-def test_send_tensor():
     # Ввести в поиск Тензор
     page.search_field().send_keys('Тензор')
 
-def test_suggest():
     # Проверить, что появилась таблица с подсказками (suggest)
     page.suggest()
 
-def test_enter():
     # Нажать enter
     page.search_field().send_keys("\n")
 
-def test_search_results():
     # Проверить, что появилась страница результатов поиска
     label_results = page.search_results()
     assert label_results == 'Результаты поиска', "Страница результатов поиска не открылась"
 
-def test_first_result():
     # Проверить 1 ссылка ведет на сайт tensor.ru
     first_url = page.first_url()
     assert first_url == 'https://tensor.ru/', "Первая ссылка ведет не на Тензор"
-
-
